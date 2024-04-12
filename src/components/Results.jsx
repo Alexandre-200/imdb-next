db6f25bbdfd69b8d43b0ed42e5fffa4855f4765d
Alexandre-200 <alexandre_200@outlook.com.br>
2024-04-12 20:04:24 -0300

    adicionar  efeitos de loading

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,16 @@
+
+
 export default function Results({ results }) {
+   // console.log("results ",results)
     return (
         <div>
             {results.map((result) => {
 
                 <div key={result.id}>
                     <h2>{result.id}</h2>
+                    {
+                        console.log((result.id))
+                    }
                 </div>
             })}
         </div>
