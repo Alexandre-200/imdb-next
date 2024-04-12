d1f4ff8306ba416bd068508de1c5aac6f011a744
Alexandre-200 <alexandre_200@outlook.com.br>
2024-04-11 23:06:42 -0300

    adicionado pagina sobre

---
+++ src/app/about/page.jsx
@@ -0,0 +1,25 @@
+export default function page() {
+    return (
+        <div className="max-w-6xl mx-auto p-3 space-y-4">
+            <h1 className="text-2xl font-medium text-amber-600">About</h1>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Dignissimos mollitia corporis architecto necessitatibus,
+                ex omnis? Error sunt facere possimus delectus qui explicabo
+                voluptate rerum, est minima fugiat quas exercitationem hic.
+            </p>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Dignissimos mollitia corporis architecto necessitatibus,
+                ex omnis? Error sunt facere possimus delectus qui explicabo
+                voluptate rerum, est minima fugiat quas exercitationem hic.
+            </p>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Dignissimos mollitia corporis architecto necessitatibus,
+                ex omnis? Error sunt facere possimus delectus qui explicabo
+                voluptate rerum, est minima fugiat quas exercitationem hic.
+            </p>
+        </div>
+    )
+}
