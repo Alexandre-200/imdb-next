9cde4a756afc0e4ee6e3810535bd67eeb29e1d88
Alexandre-200 <alexandre_200@outlook.com.br>
2024-04-12 18:01:11 -0300

    captura de error

---
+++ src/app/error.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default function error({ error, reset }) {
+
+    useEffect(() => {
+
+    }, [error])
+    return (
+        <div className="text-center mt-10">
+            <h1>Algo deu errado, por favor tente novamente mais tarde.</h1>
+            <button className="hover:text-amber-600 mt-5" onClick={()=>reset()}>Tentar Novamente</button>
+        </div>
+    )
+}
